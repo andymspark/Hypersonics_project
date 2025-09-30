e6e4e509860b9f13b0b2c5eb4f6c9e81d8730a27
fosherrat <138839137+fosherrat@users.noreply.github.com>
2025-09-30 16:17:53 +0900

    delete

--- tests/test_relations.py
+++
@@ -1,8 +0,0 @@
-from ..src.aero.normal_shock import normal_shock_ratios
-
-def test_normal_shock_basic():
-    r = normal_shock_ratios(7.0, gamma=1.4)
-    assert r['M2'] < 1.0
-    assert r['p2_p1'] > 1.0
-    assert r['rho2_rho1'] > 1.0
-    assert r['T2_T1'] > 1.0
